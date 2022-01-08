6150a5242c9c73f4b1e0dd7455e3e79b89fc5cff
royweb <xytank@gmail.com>
2022-01-08 15:15:30 -0500

    Add contract function to burn tokens

--- NFTContract.sol
+++ NFTContract.sol
@@ -1,4 +1,5 @@
-param solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 // Import contracts from Openzepplin
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
@@ -9,7 +10,7 @@ contract NFTContract is ERC1155, Ownable {
     uint256 public constant ARTWORK = 0;
     uint256 public constant PHOTO = 1;
 
-    contructor() ERC1155("url") {
+    constructor() ERC1155("url") {
         // '_mint()' is from ERC1155 openzeppelin contract
         _mint(msg.sender, ARTWORK, 1, "");
         _mint(msg.sender, PHOTO, 2, "");
@@ -19,9 +20,15 @@ contract NFTContract is ERC1155, Ownable {
     // 'onlyOwner' is from Ownable openzeppelin contract and using it as modifier
     // Modifier is a snippet of code that gets run before this function gets run
     // 'onlyOwner' make sure that the caller is actually equal to the owner
-    function mint(address account, uint256 id, uint256amount) public onlyOwner{
+    function mint(address account, uint256 id, uint256 amount) public onlyOwner{
         // Make sure that this function can only be called by the owner
-        require();
         _mint(account, id, amount, "");
+    }
+    
+    // Burn tokens
+    function burn(address account, uint256 id, uint256 amount) public {
+        // Make sure that this function can only be called by the token owner
+        require(msg.sender == account);
+        _burn(account, id, amount);
     }
 }
