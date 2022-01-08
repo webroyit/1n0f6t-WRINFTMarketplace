15e8f0c6589a0e9b75f6de8309150c01c97947a6
royweb <xytank@gmail.com>
2022-01-08 13:50:01 -0500

    Add onlyOwner from openzeppelin contract to the mint() as modifier

--- NFTContract.sol
+++ NFTContract.sol
@@ -1,21 +1,27 @@
 param solidity ^0.8.0;
 
-// Import ERC1155 token contract from Openzepplin
+// Import contracts from Openzepplin
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-// 'is' inherit ERC1155 openzeppelin contract
-contract NFTContract is ERC1155 {
+// 'is' inherit ERC1155 and Ownable openzeppelin contract
+contract NFTContract is ERC1155, Ownable {
     uint256 public constant ARTWORK = 0;
     uint256 public constant PHOTO = 1;
 
     contructor() ERC1155("url") {
-        //_mint() is from ERC1155 openzeppelin contract
+        // '_mint()' is from ERC1155 openzeppelin contract
         _mint(msg.sender, ARTWORK, 1, "");
         _mint(msg.sender, PHOTO, 2, "");
     }
 
-    // Mint Tokens
-    function mint(address account, uint256 id, uint256amount) public {
+    // Mint tokens
+    // 'onlyOwner' is from Ownable openzeppelin contract and using it as modifier
+    // Modifier is a snippet of code that gets run before this function gets run
+    // 'onlyOwner' make sure that the caller is actually equal to the owner
+    function mint(address account, uint256 id, uint256amount) public onlyOwner{
+        // Make sure that this function can only be called by the owner
+        require();
         _mint(account, id, amount, "");
     }
 }
