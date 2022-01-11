2b66f16ffb143108b3e40720ad870839b30150e2
royweb <xytank@gmail.com>
2022-01-11 15:36:02 -0500

    Login with Moralis

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 console.log("It works");
 
 // Connect to Moralis server
-Moralis.initialize("ntozI14MGOaGCCPaTsw5ZzKyqzzO8wcpMzalWpLQ");
-Moralis.serverURL("https://9qnksoon7okz.usemoralis.com:2053/server");
+Moralis.initialize("yOGw5F1DxY2eA9ByFSlvoze95s9woOjLcHzxgm9Y");
+Moralis.serverURL = "https://okraypjofzst.usemoralis.com:2053/server";
+
+// Check if the user is login
+async function initializeApp(){
+    let currentUser = Moralis.User.current();
+    if(!currentUser){
+        currentUser = await Moralis.Web3.authenticate();
+    }
+    console.log(currentUser);
+}
+
+initializeApp();
