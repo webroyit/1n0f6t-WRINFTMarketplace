266da2a288ff7e2dc2b76c8846f33854587b22c5
royweb <xytank@gmail.com>
2022-01-11 14:32:23 -0500

    Set up connection to Moralis server

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log("It works");
+
+// Connect to Moralis server
+Moralis.initialize("ntozI14MGOaGCCPaTsw5ZzKyqzzO8wcpMzalWpLQ");
+Moralis.serverURL("https://9qnksoon7okz.usemoralis.com:2053/server");
