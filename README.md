d6eeb58e4fa9b6a63a9dc0b29d3e869ac11ce947
royweb <xytank@gmail.com>
2022-01-13 16:02:37 -0500

    Add function to fetch metadata from URL

--- README.md
+++ README.md
@@ -3,5 +3,28 @@
 ## View Metadata on Open Sea
 - Go to https://opensea.io/get-listed
 
+## Add cloud function in Moralis
+```
+// Name of the function
+Moralis.Cloud.define("getNFT", async (request) => {
+    // console.log()
+    const logger = Moralis.Cloud.getLogger();
+
+    let NFTId = request.params.nftId;
+    let hexId = parseInt(NFTId).toString(16); // Convert NFT Id into a hex Id
+    let paddedHex = ("0000000000000000000000000000000000000000000000000000000000000000" + hexId).slice(-64);
+
+    logger.info(paddedHex);
+
+    return Moralis.Cloud.httpRequest({ url: "https://000e155m12kundcrkeh72ckooc5c5uvp6ie5dmnhdphiclcdfbjpijg.siasky.net/" + paddedHex + ".json"})
+        .then(function(httpResponse){
+            return httpResponse.text;
+        },function(httpResponse) {
+            // error
+            console.error('Request failed with response code ' + httpResponse.status);
+        })
+});
+```
+
 ## Notes
 - Metadata must follow this standard. https://eips.ethereum.org/EIPS/eip-1155#metadata
--- main.js
+++ main.js
@@ -4,6 +4,18 @@ console.log("It works");
 Moralis.initialize("yOGw5F1DxY2eA9ByFSlvoze95s9woOjLcHzxgm9Y");
 Moralis.serverURL = "https://okraypjofzst.usemoralis.com:2053/server";
 
+function fetchMetadata(NFTs){
+    for(let i = 0; i < NFTs.length; i++){
+         let nft = NFTs[i];
+         let id = nft.token_id;
+
+         // Call Moralis cloud function -> Static JSON file
+         fetch("https://okraypjofzst.usemoralis.com:2053/server/functions/getNFT?_ApplicationId=yOGw5F1DxY2eA9ByFSlvoze95s9woOjLcHzxgm9Y&nftId=" + id)
+            .then(res => res.json())
+            .then(res => console.log(res));
+    }
+}
+
 // Check if the user is login
 async function initializeApp(){
     let currentUser = Moralis.User.current();
@@ -11,9 +23,11 @@ async function initializeApp(){
         currentUser = await Moralis.Web3.authenticate();
     }
     
+    // Get NFTs data
     const options = { address: "0xbcbD0331394bcCEFbfC93C9355d362C798180d0e", chain: "rinkeby"}
     let NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
     console.log(NFTs);
+    fetchMetadata(NFTs.result);
 }
 
 initializeApp();
