c47cb88f88eecac5594b16d6a8a2e82c2b61b7f8
royweb <xytank@gmail.com>
2022-01-14 14:27:08 -0500

    Use Promise.all()

--- main.js
+++ main.js
@@ -4,18 +4,24 @@ console.log("It works");
 Moralis.initialize("yOGw5F1DxY2eA9ByFSlvoze95s9woOjLcHzxgm9Y");
 Moralis.serverURL = "https://okraypjofzst.usemoralis.com:2053/server";
 
+// Fetch metadata from URL
 function fetchMetadata(NFTs){
+    let promises = [];
+    
     for(let i = 0; i < NFTs.length; i++){
          let nft = NFTs[i];
          let id = nft.token_id;
 
          // Call Moralis cloud function -> Static JSON file
-         fetch("https://okraypjofzst.usemoralis.com:2053/server/functions/getNFT?_ApplicationId=yOGw5F1DxY2eA9ByFSlvoze95s9woOjLcHzxgm9Y&nftId=" + id)
+         promises.push(fetch("https://okraypjofzst.usemoralis.com:2053/server/functions/getNFT?_ApplicationId=yOGw5F1DxY2eA9ByFSlvoze95s9woOjLcHzxgm9Y&nftId=" + id)
             .then(res => res.json())
             // Convert string into object
             .then(res => JSON.parse(res.result))
-            .then(res => console.log(res));
+            .then(res => {nft.metadata = res})
+            .then(() => {return nft;}))
     }
+
+    return Promise.all(promises);
 }
 
 // Check if the user is login
@@ -28,8 +34,8 @@ async function initializeApp(){
     // Get NFTs data
     const options = { address: "0xbcbD0331394bcCEFbfC93C9355d362C798180d0e", chain: "rinkeby"}
     let NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
-    console.log(NFTs);
-    fetchMetadata(NFTs.result);
+    let NFTWithMetadata = await fetchMetadata(NFTs.result);
+    console.log(NFTWithMetadata);
 }
 
 initializeApp();
