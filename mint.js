24a30a6c4ce0a65b0fcab975cd8c26a7f8207794
royweb <xytank@gmail.com>
2022-01-19 14:27:31 -0500

    Get nftId from param of URL

---
+++ mint.js
@@ -0,0 +1,21 @@
+// Connect to Moralis server
+Moralis.initialize("yOGw5F1DxY2eA9ByFSlvoze95s9woOjLcHzxgm9Y");
+Moralis.serverURL = "https://okraypjofzst.usemoralis.com:2053/server";
+const CONTRACT_ADDRESS = "0xbcbD0331394bcCEFbfC93C9355d362C798180d0e";
+
+// Check if the user is login
+async function initializeApp(){
+    let currentUser = Moralis.User.current();
+    if(!currentUser){
+        // Redirect
+        window.location.pathname = "/index.html";
+    }
+    
+    // Get NFT data
+    // Get the params from the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const nftId = urlParams.get("nftId");
+    console.log(nftId);
+}
+
+initializeApp();
